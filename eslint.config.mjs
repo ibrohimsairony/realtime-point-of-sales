96636c1a9186b07e12a3b20dd97825127cbeb7b7
ibrohimsairony <ibrohimsairony@gmail.com>
2025-08-09 18:55:18 +0700

    Initial commit from Create Next App

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+eslintConfig.push({
+  rules: {
+    //! Jangan sampai typo
+    "react-hooks/exhautive-deps": "off",
+    "react-/jsx-key": "off",
+    '@typescript-eslint/no-explicit-any' : 'off',
+    '@typescript-eslint/no-unsed-vars' : 'off'
+  },
+});
+export default eslintConfig;
